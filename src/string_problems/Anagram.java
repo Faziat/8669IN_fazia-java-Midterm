287c8443d76985c9e9071aaa9eb8fac1b290e40a
Faziat <toubouachefazia@gmail.com>
2023-05-30 23:38:58 -0400

    Anagram Class

--- src/string_problems/Anagram.java
+++ src/string_problems/Anagram.java
@@ -1,6 +1,13 @@
 package string_problems;
 
+import java.util.Arrays;
+
 public class Anagram {
+    public static void main(String[] args) {
+
+        checkAnagram();
+
+    }
 
     /** INSTRUCTIONS
      * Write a Java Program, `isAnagram`, to check if any two strings are anagrams
@@ -14,5 +21,40 @@ public class Anagram {
      */
 
     // Implement Here
+    public static void checkAnagram (){
+
+        String str1 = "Debit CARD"; // {D, E, B, I, T, C, A, R, D} --> A B C D D E I R T
+        String str2 = "BAD Credit"; // {B, A, D, C, R, E, D, I, T} --> A B C D D E I R T
+
+        str1 = str1.toUpperCase();
+        str2 = str2.toUpperCase();
+
+        if(str1.length() == str2.length()){
+
+            char[] charArray1 = str1.toCharArray();
+            char[] charArray2 = str2.toCharArray();
+
+            Arrays.sort(charArray1);
+            Arrays.sort(charArray2);
+
+            boolean isAnagram = Arrays.equals(charArray1,charArray2);
+
+            if (isAnagram){
+
+                System.out.println(str1 + " and " + str2 + " are Anagrams");
+
+            } else {
+
+                System.out.println(str1 + " and " + str2 + " are not anagrams");
+
+            }
+
+        } else {
+
+            System.out.println(str1 + " and " + str2 + " are not anagrams");
+
+        }
+
+    }
 
 }
