1043071d48223bc3598e127dd7b35648cc028a52
Faziat <toubouachefazia@gmail.com>
2023-05-31 00:49:49 -0400

    Duplicate Word class

--- src/string_problems/DuplicateWord.java
+++ src/string_problems/DuplicateWord.java
@@ -2,7 +2,8 @@ package string_problems;
 
 public class DuplicateWord {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a java program to find duplicate words and the number of occurrences of those words in the given string.
      * Also, find the average length of all the words
      */
@@ -11,7 +12,30 @@ public class DuplicateWord {
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
 
         // Implement here
+        int count;
+        //Converts the string into lowercase
+        st = st.toLowerCase();
+        //Split the string into splittedString using built-in function
+        String[] splittedString = st.split(" ");
 
+        for (int i = 0; i < splittedString.length; i++) {
+            count = 1;
+            for (int j = i + 1; j < splittedString.length; j++) {
+                if (splittedString[i].equals(splittedString[j])) {
+                    count++;
+                    //Set splittedString[j] to 0 to avoid printing visited word
+                    splittedString[j] = "0";
+                }
+            }
+
+        if (count > 1 && splittedString[i] != "0")
+            System.out.println(splittedString[i] + " with occurrences: " + (count - 1));
+        }
+
+        int avgLength = st.replace(" ","").length() / splittedString.length;
+
+        System.out.println("Average length of all the words is " + avgLength);
     }
 
 }
+
