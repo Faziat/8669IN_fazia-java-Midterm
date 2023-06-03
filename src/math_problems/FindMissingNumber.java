80dbbedd8a011a754dcae49335f443167cd59cad
Faziat <toubouachefazia@gmail.com>
2023-06-03 00:08:20 -0400

    Prime class

--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -1,5 +1,7 @@
 package math_problems;
 
+import java.util.Arrays;
+
 public class FindMissingNumber {
 
     /** INSTRUCTIONS
@@ -8,6 +10,40 @@ public class FindMissingNumber {
 
     public static void main(String[] args) {
         int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
+        int missingNumber = findMissingNumber(array);
+        System.out.println("The missing number is: " + missingNumber);
 
+        System.out.println("The missing number is: " + findMissingNumber2(array));
+    }
+    public static int findMissingNumber(int[] arr) {
+        int n = arr.length + 1;
+        int sum = n * (n + 1) / 2;
+        int arrSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arrSum += arr[i];
+        }
+        return sum - arrSum;
+    }
+
+    public static int findMissingNumber2(int[] arr2){
+        int[] arrTest = arr2;
+
+        Arrays.sort(arrTest);
+
+        int missingNumber = 0;
+
+        int diff = 0;
+
+        for (int i = 0; i<arrTest.length;i++){
+
+            diff = arrTest[i+1] - arrTest[i];
+
+            if (diff > 1){
+                missingNumber = i + 2;
+                break;
+            }
+
+        }
+        return missingNumber;
     }
 }
