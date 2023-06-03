52721ff212f3239a3f57203c17cda61765106c81
Faziat <toubouachefazia@gmail.com>
2023-06-02 21:08:23 -0400

    Permutation class

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -2,14 +2,49 @@ package string_problems;
 
 public class Permutation {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Create a Java program to compute all permutations of any given string
-     *
+     * <p>
      * e.g. -  "ABC" = "ABC" "ACB" "BAC" "BCA" "CAB" "CBA"
      */
 
     public static void main(String[] args) {
+        String str = "ABC";
+        int len = str.length();
+        System.out.println("All the permutations of the string are: ");
+        generatePermutation(str, 0, len);
+
 
     }
+
     // Implement Here
-}
+    public static String swapString(String a, int i, int j) {
+        char[] b = a.toCharArray();
+        char ch;
+        ch = b[i];
+        b[i] = b[j];
+        b[j] = ch;
+        return String.valueOf(b);
+    }
+
+        public static void generatePermutation (String str,int start, int end){
+
+
+            if (start == end - 1)
+                System.out.println(str);
+            else {
+                for (int i = start; i < end; i++) {
+
+                    str = swapString(str, start, i);
+
+                    generatePermutation(str, start + 1, end);
+
+                    str = swapString(str, start, i);
+                }
+            }
+        }
+    }
+
+
+
