856a6b40ff383cf89180b73a2eb56b9aa07b68e6
Faziat <toubouachefazia@gmail.com>
2023-06-02 21:26:09 -0400

    Fibonacci class

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -10,5 +10,27 @@ public class Fibonacci {
 
     public static void main(String[] args) {
 
+        int maxNumber = 40;
+        int previousNumber = 0;
+        int nextNumber = 1;
+
+        System.out.print("Fibonacci Series of "+maxNumber+" numbers:");
+
+        for (int i = 0; i <= maxNumber; ++i)
+        {
+            System.out.println("Fibonacci " + i + ": " + previousNumber);
+            /* On each iteration, we are assigning second number
+             * to the first number and assigning the sum of last two
+             * numbers to the second number
+             */
+
+
+            int sum = previousNumber + nextNumber;
+            previousNumber = nextNumber;
+            nextNumber = sum;
+        }
     }
 }
+
+
+
